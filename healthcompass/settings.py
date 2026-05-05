922173f2d5aa651204e79615224af78a610a9cc0
seyedhasanmirhoseini1367 <seyedhasan.mirhoseini1367@gmail.com>
2026-05-05 14:20:42 +0300

    feat: Cloudinary media storage for persistent profile photos in production

--- healthcompass/settings.py
+++ healthcompass/settings.py
@@ -13,6 +13,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv(
 # Trust Railway's HTTPS proxy and the subdomain
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='', cast=Csv())
 
+CLOUDINARY_URL = config('CLOUDINARY_URL', default='')
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -20,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'cloudinary_storage',
     # Project apps
     'accounts',
     'medical_records',
@@ -97,6 +101,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Use Cloudinary for media uploads in production; local filesystem locally
+if CLOUDINARY_URL:
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
