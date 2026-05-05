44c330167c78432df56ed629af75f58b2deaabb4
seyedhasanmirhoseini1367 <seyedhasan.mirhoseini1367@gmail.com>
2026-05-05 17:28:59 +0300

    fix: replace static() helper with explicit serve_media view for production

--- healthcompass/urls.py
+++ healthcompass/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import FileResponse, Http404
 from .home_view import home
+import os
+
+
+def serve_media(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.isfile(file_path):
+        return FileResponse(open(file_path, 'rb'))
+    raise Http404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,9 +25,7 @@ urlpatterns = [
     path('stories/', include('stories.urls')),
     path('notifications/', include('notifications.urls')),
     path('integrations/', include('integrations.urls')),
+    re_path(r'^media/(?P<path>.+)$', serve_media),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Serve media files in all environments (volume-backed in production)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
